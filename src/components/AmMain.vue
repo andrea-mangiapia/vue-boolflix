16f8a9229c95b7947c28b15e093c7e58b26fae8d
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-07-14 18:27:25 +0200

    milestone 3 - add poster

--- src/components/AmMain.vue
+++ src/components/AmMain.vue
@@ -7,6 +7,10 @@
                     <div>Titolo: {{film.title}}</div>
                     <div>Titolo originale: {{film.original_title}}</div>
                     <div>
+                        <img v-if="film.poster_path" class="poster" :src="`https://image.tmdb.org/t/p/w342${film.poster_path}`" :alt="film.original_language">
+                        <img v-else class="poster" src="../assets/style/img/img-placeholder.jpg" alt="Image Default">
+                    </div>
+                    <div>
                         Lingua: 
                         <img v-if="availableFlags.includes(film.original_language)" class="flag" :src="require(`../assets/style/img/${film.original_language}.png`)" :alt="film.original_language">
                         <span v-else>{{film.original_language}}</span>
@@ -23,8 +27,12 @@
                     <div>Titolo: {{serie.name}}</div>
                     <div>Titolo originale: {{serie.original_name}}</div>
                     <div>
+                        <img v-if="serie.poster_path" class="poster" :src="`https://image.tmdb.org/t/p/w342${serie.poster_path}`" :alt="serie.original_language">
+                        <img v-else class="poster" src="../assets/style/img/img-placeholder.jpg" alt="Image Default">
+                    </div>
+                    <div>
                         Lingua:
-                        <img v-if="availableFlags.includes(serie.original_language)" class="flag" :src="require(`../assets/style/img/${serie.original_language}.png`)" alt="">
+                        <img v-if="availableFlags.includes(serie.original_language)" class="flag" :src="require(`../assets/style/img/${serie.original_language}.png`)" :alt="serie.original_language">
                         <span v-else>{{serie.original_language}}</span>
                     </div>
                     <div>Voto: {{serie.vote_average}}</div>
@@ -69,11 +77,15 @@ section {
     li {
         padding: 15px 10px;
         background-color: white;
-        
-    }
 
-    .flag {
+        .flag {
         width: 25px;
+        }
+
+        .poster {
+        width: 220px;
+        }
+        
     }
 }
 </style>
